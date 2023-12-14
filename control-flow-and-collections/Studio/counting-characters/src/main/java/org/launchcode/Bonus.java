c51e6dd3de38935efd4d432868be6dbdc53da1c9
unknown <ryjwink@gmail.com>
2023-12-13 21:46:17 -0600

    Control flow studio

--- control-flow-and-collections/Studio/counting-characters/src/main/java/org/launchcode/Bonus.java
+++ control-flow-and-collections/Studio/counting-characters/src/main/java/org/launchcode/Bonus.java
@@ -1,2 +1,39 @@
-package org.launchcode;public class Bonus {
+package org.launchcode;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Bonus {
+
+    public static void main(String[] args) {
+
+        String userQuote;
+        Scanner input;
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        input = new Scanner(System.in);
+        System.out.println("Please enter a string of characters: ");
+        userQuote = input.nextLine();
+
+        char[] charactersArray = userQuote.toLowerCase().toCharArray();
+
+        HashMap<Character,Integer> counts = new HashMap<>();
+
+        for ( char words : charactersArray) {
+          if (alphabet.indexOf(words) >= 0) {
+              if (counts.containsKey(words)) {
+                  counts.put(words, counts.get(words) + 1);
+              } else {
+                  counts.put(words, 1);
+              }
+          }
+        }
+
+        for (Map.Entry<Character,Integer> count : counts.entrySet()) {
+            System.out.println(count.getKey()+ ": " + count.getValue());
+        }
+
+    }
 }
--- control-flow-and-collections/Studio/counting-characters/src/main/java/org/launchcode/CharacterCount.java
+++ control-flow-and-collections/Studio/counting-characters/src/main/java/org/launchcode/CharacterCount.java
@@ -1,2 +1,31 @@
-package org.launchcode;public class CharacterCount {
+package org.launchcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CharacterCount {
+
+    public static void main(String[] args) {
+        String quote = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with." +
+                " So if you move all the terms over to one side, " +
+                "you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. " +
+                "Once you’ve done that, it’s pretty straightforward from there.";
+
+        char[] charactersArray = quote.toCharArray();
+
+        HashMap<Character,Integer> counts = new HashMap<>();
+
+        for ( char words : charactersArray) {
+            if ( counts.containsKey(words)) {
+                counts.put(words, counts.get(words) + 1);
+            } else {
+                counts.put(words,1);
+            }
+        }
+
+        for (Map.Entry<Character,Integer> count : counts.entrySet()) {
+            System.out.println(count.getKey()+ ": " + count.getValue());
+        }
+
+    }
 }
